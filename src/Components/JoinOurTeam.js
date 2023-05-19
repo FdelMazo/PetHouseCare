7877391012df78738826553b59a3d64dd0e8a245
Federico del Mazo <FdelMazo@fi.uba.ar>
2023-05-19 11:34:03 -0300

    Make two join buttons instead of one

--- src/Components/JoinOurTeam.js
+++ src/Components/JoinOurTeam.js
@@ -1,14 +1,13 @@
 import {
+    Text,
     Box,
     Stack,
     Heading,
-    Text,
     Container,
     Input,
     Button,
     SimpleGrid,
     ButtonGroup,
-    IconButton,
     Tooltip,
     useColorModeValue,
 } from '@chakra-ui/react';
@@ -29,20 +28,15 @@ export const JoinOurTeam = () => {
     const [error, setError] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [role, setRole] = useState('');
     const {setUser} = useUser();
 
-    async function register() {
+    async function register(role) {
         if (!username) {
             setError('Username cannot be empty');
             return;
         }
         if (!password) {
             setError('Password cannot be empty');
-            return;
-        }
-        if (!role) {
-            setError('Role cannot be empty');
             return;
         }
         setError('');
@@ -94,7 +88,10 @@ export const JoinOurTeam = () => {
                 columns={{ base: 1, md: 2 }}
                 spacing={{ base: 10, lg: 32 }}
                 py={{ base: 10, sm: 20, lg: 32 }}>
-                <Stack spacing={{ base: 10, md: 20 }}>
+                <Stack
+                    spacing={{ base: 10, md: 20 }}
+                    justifyContent={'center'}
+                >
                     <Heading
                         lineHeight={1.1}
                         fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}>
@@ -106,40 +103,38 @@ export const JoinOurTeam = () => {
                             world
                         </Text>
                     </Heading>
-                    <Stack direction={'row'} spacing={4} align={'center'}>
-                    </Stack>
                 </Stack>
                 <Stack
                     bg={'gray.50'}
                     rounded={'xl'}
-                    p={{ base: 4, sm: 6, md: 8 }}
-                    spacing={{ base: 8 }}
-                    maxW={{ lg: 'lg' }}>
-                    <Stack spacing={4}>
-                        <Heading
-                            color={'gray.800'}
-                            lineHeight={1.1}
-                            fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
-                            Join PetHouseCare
-                            <Text
-                                as={'span'}
-                                bgGradient='linear(to-r, red.400,pink.400)'
-                                bgClip='text'>
-                                !
-                            </Text>
-                        </Heading>
-                    </Stack>
-                    <Box as={'form'} mt={10}>
+                    pb={4}
+                    pt={8}
+                    px={8}
+                    spacing={2}
+                    maxW={'lg'}
+                    margin={'auto'}
+                >
+                    <Heading
+                        color={'gray.800'}
+                        fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
+                        Join PetHouseCare
+                        <Text
+                            as={'span'}
+                            bgGradient='linear(to-r, red.400,pink.400)'
+                            bgClip='text'>
+                            !
+                        </Text>
+                    </Heading>
+                    <Text
+                        fontFamily={'heading'}
+                        color="red"
+                        fontSize="smaller"
+                        mb="12px !important"
+                    >
+                        {error || <br />}
+                    </Text>
+                    <Box as={'form'}>
                         <Stack spacing={4}>
-                            {error &&
-                                <Text
-                                    fontFamily={'heading'}
-                                    color="red"
-                                    fontSize="smaller"
-                                >
-                                    {error}
-                                </Text>
-                            }
                             <Input
                                 onChange={(event) => setUsername(event.target.value)}
                                 placeholder='Username'
@@ -157,7 +152,6 @@ export const JoinOurTeam = () => {
                                 bg={'gray.100'}
                                 border={0}
                                 color={'gray.500'}
-
                                 _placeholder={{
                                     color: 'gray.500',
                                 }}
@@ -166,72 +160,65 @@ export const JoinOurTeam = () => {
                             <ButtonGroup
                                 justifyContent={'center'}
                                 isAttached
+                                w={'full'}
                                 color={useColorModeValue('var(--chakra-colors-chakra-body-text)', 'gray.800')}
                             >
                                 <Tooltip
                                     closeOnClick={false}
                                     label={<Text textAlign={"center"}>I'm a home owner <br />with pets to take care of</Text>}
                                     hasArrow
-                                    placement={'left'}
+                                    placement={'top'}
                                 >
-                                    <IconButton
-                                        {...(role === Roles.DUEÑO ? {
-                                            _hover: { bgGradient: 'linear(to-r, red.400,pink.400)' },
+                                    <Button
+                                        onClick={async () => {
+                                            await register(Roles.DUEÑO);
+                                        }}
+                                        _hover={{
                                             bgGradient: 'linear(to-r, red.400,pink.400)',
-                                        } : {})}
-                                        onClick={() => setRole(Roles.DUEÑO)}
-                                        icon={<IoIosHome />}
-                                        {...(!role && {
-                                            borderRight: '1px dashed lightslategray'
-                                        })}
-                                    />
+                                            boxShadow: 'xl',
+                                        }}
+                                        leftIcon={<IoIosHome />}
+                                        borderRight='1px dashed lightslategray'
+                                    >
+                                        <Text>Join as a homeowner</Text>
+                                    </Button>
                                 </Tooltip>
                                 <Tooltip
                                     closeOnClick={false}
                                     label={<Text textAlign={"center"}>I'm a world traveller <br />pet care taker</Text>}
                                     hasArrow
-                                    placement={'right'}>
-                                    <IconButton
-                                        {...(role === Roles.CUIDADOR ? {
-                                            _hover: { bgGradient: 'linear(to-r, red.400,pink.400)' },
+                                    placement={'top'}>
+                                    <Button
+                                        onClick={async () => {
+                                            await register(Roles.CUIDADOR);
+                                        }}
+                                        _hover={{
                                             bgGradient: 'linear(to-r, red.400,pink.400)',
-                                        } : {})}
-                                        onClick={() => setRole(Roles.CUIDADOR)}
-                                        icon={<IoIosPaw />}
-                                    />
+                                            boxShadow: 'xl',
+                                        }}
+                                        borderLeft='1px dashed lightslategray'
+                                        rightIcon={<IoIosPaw />}
+                                    >
+                                        Join as a caretaker
+                                    </Button>
                                 </Tooltip>
                             </ButtonGroup>
                         </Stack>
 
                         <Button
-                            onClick={async () => {
-                                await register();
-                            }}
-                            fontFamily={'heading'}
-                            mt={4}
-                            w={'full'}
-                            bgGradient='linear(to-r, red.400,pink.400)'
-                            color={'white'}
-                            _hover={{
-                                bgGradient: 'linear(to-r, red.400,pink.400)',
-                                boxShadow: 'xl',
-                            }}>
-                            Join now
-                        </Button>
-                        <Button
+                            alignSelf={'flex-end'}
                             onClick={async () => {
                                 await login();
                             }}
-                            fontFamily={'heading'}
-                            mt={2}
+                            mt={8}
+                            variant={'link'}
                             colorScheme="gray"
                             w={"20ch"}
                             color={useColorModeValue('var(--chakra-colors-chakra-body-text)', 'gray.800')}
                         >
-                            Log in
+                            Log in instead
                         </Button>
                     </Box>
-                    form
                 </Stack>
             </Container>
         </Box>
