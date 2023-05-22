938c2314c5a32af1f098be9332fd2791989d7994
Nelson Ramos <nramos@fi.uba.ar>
2023-05-22 15:54:15 -0300

    Se agrego el filtro por ubicación a la lista de hogares.

--- src/Components/ListaHogares.js
+++ src/Components/ListaHogares.js
@@ -1,5 +1,5 @@
 import TarjetaHogar from './TarjetaHogar';
-import { Container, useColorModeValue } from '@chakra-ui/react';
+import { Input, Container, useColorModeValue } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { db, ROLES } from '../db';
 import { Navbar } from './Navbar';
@@ -8,6 +8,8 @@ import { ROUTES } from '../routes';
 
 export function ListaHogares() {
     const [homeowners, sethomeowners] = useState([]);
+    const [search, setSearch] = useState("");
+
   useEffect(() => {
     const action = async () => {
         sethomeowners(await db.users.where('role').equals(ROLES.DUEÑO).toArray());
@@ -15,11 +17,25 @@ export function ListaHogares() {
     action();
   }, []);
 
+  let result = [];
+  if (!search){
+    result = homeowners;
+  } else {
+    result = homeowners.filter ( (homeowner) => 
+                homeowner.home.location.toLowerCase().includes(search.toLowerCase())
+    )
+  }
+
+  const searcher = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
       <>
           <Navbar currentRoute={ROUTES.HOMEOWNERS} />
           <Container bg={useColorModeValue("gray.100", "gray.700")} centerContent p={10} borderRadius={2} maxW="80ch" h="fit-content">
-              {homeowners.map((homeowner) => {
+            <Input bg="white" color="gray.800" placeholder='Ubicación' value={search} onChange={searcher}/>
+              {result.map((homeowner) => {
                   return <TarjetaHogar homeowner={homeowner} key={homeowner.username + homeowner.password} />
               })}
           </Container>
